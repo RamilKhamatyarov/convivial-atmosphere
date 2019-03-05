03ac6a6aa41236f84920a201a52e4eb7083fc4fd
RamilKhamatyarov <rraamil@gmail.com>
2019-03-05 19:16:50 +0300

    Add Rabbit MQ configuration and spring AMQP dependency.

---
+++ src/main/java/ru/rkhamatyarov/convivialatmosphere/configuration/TopicExchange.java
@@ -0,0 +1,10 @@
+package ru.rkhamatyarov.convivialatmosphere.configuration;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TopicExchange {
+    private String exchangeName;
+}
